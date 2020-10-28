e4a762e6775c0474a7f93d4c4ec4aa64f8f378ba
Matias Bangueses <matiasluisb13@gmail.com>
2020-10-28 19:52:11 -0300

    primera funcion para la clase

--- base_de_datos.py
+++ base_de_datos.py
@@ -1,1 +1,23 @@
 import sqlite3
+
+class BaseDeDatos():
+
+    def __init__(self):
+            self.conexion = sqlite3.connect("base_de_datos.db")
+            self.cursor = self.conexion.cursor()
+
+    def comprobar_existencia(self, usuario):
+        #Si el usuario existe, devuelve su ID, sinó, devuelve -1
+        self.cursor.execute("SELECT usuario FROM usuarios")
+        lista = self.cursor.fetchall()
+        i = 0
+        presente = False
+        while i <len(lista) and presente == False:
+            if lista[i][0] == usuario:
+                presente = True
+            i += 1
+        self.conexion.commit()
+        return i if presente == True else -1
+    
+uwu = BaseDeDatos()
+print(uwu.comprobar_existencia('Gazuto Orsego#7386'))
